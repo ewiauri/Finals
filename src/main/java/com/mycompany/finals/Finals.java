a4934b55be2557444bd1bf1be09ac1d7b91fbdfa
ASUS <ASUS@LAPTOP-BK1G7C22>
2023-11-07 00:47:18 +0800

    First Commit!

---
+++ src/main/java/com/mycompany/finals/Finals.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.finals;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Finals {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
